d820773f39ea36cb2cfc893f749807e6ca648446
George Morris <gmorris1242@gmail.com>
2018-05-25 12:39:36 -0400

    add menu

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var markerOne = $(".marker-one");
 var markerTwo = $(".marker-two");
 var markerThree = $(".marker-three");
 var markerFour = $(".marker-four");
+var openMenu = $("#open-menu");
+var closeMenu = $("#close-menu");
+var menuContainer = $(".menu-container");
 
 startSlideShow()
 
@@ -31,7 +34,7 @@ markerOne.click(function(){
   image.eq(0).fadeIn();
   marker.removeClass("active");
   markerOne.addClass("active");
-})
+});
 
 markerTwo.click(function(){
   clearInterval(slideShow);
@@ -39,7 +42,7 @@ markerTwo.click(function(){
   image.eq(1).fadeIn();
   marker.removeClass("active");
   markerTwo.addClass("active");
-})
+});
 
 markerThree.click(function(){
   clearInterval(slideShow);
@@ -47,7 +50,7 @@ markerThree.click(function(){
   image.eq(2).fadeIn();
   marker.removeClass("active");
   markerThree.addClass("active");
-})
+});
 
 markerFour.click(function(){
   clearInterval(slideShow);
@@ -55,4 +58,12 @@ markerFour.click(function(){
   image.eq(3).fadeIn();
   marker.removeClass("active");
   markerFour.addClass("active");
-})
+});
+
+openMenu.click(function(){
+  menuContainer.css("top","0").css("opacity",".9");
+});
+
+closeMenu.click(function(){
+  menuContainer.css("top","-100%").css("opacity","0");
+});
